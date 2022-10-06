71fc34fe2d5024a945162ac9c0adf34a034fac00
Michael Rath <michaelrath0@gmail.com>
2022-10-06 13:47:33 -0700

    Add comments for ForumController

--- Controllers/ForumController.cs
+++ Controllers/ForumController.cs
@@ -17,12 +17,20 @@ namespace Week15Project.Controllers
         }
 
         #region Room
+        /// <summary>
+        /// Action to return all rooms currently available from database
+        /// </summary>
+        /// <returns></returns>
         public IActionResult ViewAllRooms()
         {
             var rooms = repository.GetAllRooms();
             return View(rooms);
         }
-
+        /// <summary>
+        /// Action to return room with specified roomId from database
+        /// </summary>
+        /// <param name="roomId"></param>
+        /// <returns></returns>
         public IActionResult ViewRoom(int roomId)
         {
             Console.WriteLine();
@@ -36,6 +44,13 @@ namespace Week15Project.Controllers
         #endregion
 
         #region Post
+        /// <summary>
+        /// Action takes roomId, creates new post that has this roomId associated with it,
+        /// sends new post to view. This view gives the user the option to add title and message
+        /// parameters, and then submitting the post
+        /// </summary>
+        /// <param name="roomID"></param>
+        /// <returns></returns>
         public IActionResult NewPost(int roomID)
         {
             Post newPost = new Post()
@@ -44,6 +59,13 @@ namespace Week15Project.Controllers
             };
             return View(newPost);
         }
+        /// <summary>
+        /// Action that takes new post object passed to NewPost.cshtml with title and message properties
+        /// added, then adds correct UserId, date and time of post, and whether post is locked or not,
+        /// then submits this new post to the ForumRepository for addition to the database.
+        /// </summary>
+        /// <param name="newPost"></param>
+        /// <returns></returns>
         [HttpPost]
         public IActionResult NewPost(Post newPost)
         {
@@ -60,8 +82,12 @@ namespace Week15Project.Controllers
                 return RedirectToAction("Index");
             }
         }
-
-        // Action that gets all data necessary to display an individual post
+        /// <summary>
+        /// Action that returns a PostViewModel to a view, so that the post can be displayed along
+        /// with all responses to that post.
+        /// </summary>
+        /// <param name="postId"></param>
+        /// <returns></returns>
         public IActionResult ViewPost(int postId)
         {
             PostViewModel model = new PostViewModel()
@@ -71,7 +97,13 @@ namespace Week15Project.Controllers
             };
             return View(model);
         }
-        // new response
+        /// <summary>
+        /// Action takes postID, creates new response that has this postID associated with it,
+        /// sends new response to view. This view gives the user the option to add message
+        /// parameter, and then submitting the response
+        /// </summary>
+        /// <param name="postID"></param>
+        /// <returns></returns>
         public IActionResult NewResponse(int postID)
         {
             Response response = new Response()
@@ -80,7 +112,13 @@ namespace Week15Project.Controllers
             };
             return View(response);
         }
-
+        /// <summary>
+        /// Action that takes new response object passed to NewResponse.cshtml with message property
+        /// added, then adds correct UserId, date and time of post, then submits this new post to 
+        /// the ForumRepository for addition to the database.
+        /// </summary>
+        /// <param name="newResponse"></param>
+        /// <returns></returns>
         [HttpPost]
         public IActionResult NewResponse(Response newResponse)
         {
